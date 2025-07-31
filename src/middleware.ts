3c7151a9ab050739c2d3ef9fa9aa7b3b5be06e8e
Duy <duythedeveloper@gmail.com>
2025-07-30 22:52:42 -0400

    wip: broken css tho

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,21 @@
 import createMiddleware from 'next-intl/middleware'
+import { type NextRequest } from 'next/server'
+import { I18N_CONFIG } from './i18n/config'
 import { routing } from './i18n/routing'
+import { updateSession } from './utils/supabase/middleware'
 
-export default createMiddleware(routing)
+const handleI18nRouting = createMiddleware(routing)
+
+export async function middleware(request: NextRequest) {
+  const response = handleI18nRouting(request)
+
+  // A `response` can now be passed here
+  return await updateSession(request, response)
+}
 
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  // - … icon and favicon routes
-  matcher: '/((?!api|trpc|_next|_vercel|icon|favicon|.*\\..*).*)'
+  // Only run middleware on:
+  // - Root path (/) for locale detection and redirection
+  // - All localized paths (dynamically generated from i18n config)
+  matcher: ['/', `/(${I18N_CONFIG.locales.join('|')})/:path*`]
 }
